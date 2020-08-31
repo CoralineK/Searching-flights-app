0c43f8405dde5d4a74b6d179a1defe5c71e0c598
CoralineK <k.kozakiewicz.13@gmail.com>
2020-08-31 18:45:06 +0200

    Updates

---
+++ precache-manifest.c9b2f5cef0337035b39b485c13ab82f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb4a42c111f2ddfeeb499dae89509e8",
+    "url": "/CoralineK/Searching-flights-app/index.html"
+  },
+  {
+    "revision": "40cbe7e3d1d8155d1835",
+    "url": "/CoralineK/Searching-flights-app/static/css/main.7fe83ace.chunk.css"
+  },
+  {
+    "revision": "fc7a172b27deae0a4ce0",
+    "url": "/CoralineK/Searching-flights-app/static/js/2.4f153d89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CoralineK/Searching-flights-app/static/js/2.4f153d89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40cbe7e3d1d8155d1835",
+    "url": "/CoralineK/Searching-flights-app/static/js/main.fd6e1949.chunk.js"
+  },
+  {
+    "revision": "1dd0fe7ce21d0f0ca588",
+    "url": "/CoralineK/Searching-flights-app/static/js/runtime-main.2005ba2a.js"
+  },
+  {
+    "revision": "93f09e7e115ae0185c1b4b12cf625a95",
+    "url": "/CoralineK/Searching-flights-app/static/media/globe3.93f09e7e.png"
+  }
+]);
